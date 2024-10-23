de3bd7abf44ab3eb5cdbe682267d422e105628ba
biswajeet6 <biswajeetjena921@gmail.com>
2024-10-23 21:52:55 +0530

    first commit

---
+++ src/sections/Clients.jsx
@@ -0,0 +1,14 @@
+import { clientReviews } from '../constants/index.js';
+
+const Clients = () => {
+  return (
+    <section className="c-space my-20">
+
+      <div className="client-container">
+   
+      </div>
+    </section>
+  );
+};
+
+export default Clients;
